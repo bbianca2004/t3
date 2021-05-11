d0b8a01d9d3df00ab592b94e404ad832d6fd000d
Bianca Pitu <biancapitu11@gmail.com>
2021-05-11 19:43:44 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 1!");
+        }
+    }
+}

b604e227b38f629841b8830fb10435781d19ed3d
Bianca Pitu <biancapitu11@gmail.com>
2021-05-11 19:51:40 +0300

    m1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace t3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 1!");
+            Console.WriteLine("Hello World 2!");
         }
     }
 }
